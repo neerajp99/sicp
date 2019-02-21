91591323da874aa5c378ff9579df765ef939bd4c
neerajp99 <neerajp1999@gmail.com>
2019-02-21 19:37:42 +0530

    exercise 1.2

---
+++ python/ex-1.2.py
@@ -0,0 +1,11 @@
+# Exercise 1.2. 
+# Translate the following expression into prefix form
+
+x = 5 + 4 + (2 - (3 - (6 + (4 / 5))))
+y = 3 * (6 - 2) * (2 - 7)
+z = x / y
+print(z)
+
+
+#Solution
+#-0.24666666666666667
---
+++ scheme/ex-1.2.scm
@@ -0,0 +1,12 @@
+; Exercise 1.2. 
+; Translate the following expression into prefix form
+
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+	(* 3 (- 6 2) (- 7 2)))
+
+;Solution
+;-0.24666666666666667
+
+
+
