fd0a934f5983ee7da909f962b1e1f683548c17a5
neerajp99 <neerajp1999@gmail.com>
2019-04-03 23:56:33 +0530

    initial ex-1.9 in scheme

---
+++ scheme/ex-1.9.scm
@@ -0,0 +1,2 @@
+; Each of the following two procedures defines a method for adding two positive integers in terms of the procedures 'inc',
+; which increments its argument by 1, and dec, which decrements its arguments by 1.
